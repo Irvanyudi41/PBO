5d4c5a9eff8379676af632924fbe63504b276647
Irvanyudi41 <114343449+Irvanyudi41@users.noreply.github.com>
2022-11-12 20:52:33 +0700

    Add files via upload

---
+++ Tugas6/BelajarAbstract/BangunDatar.java
@@ -0,0 +1,15 @@
+
+public abstract class BangunDatar {
+    String warna;
+
+    public String getWarna() {
+        return warna;
+    }
+
+    public void setWarna(String warna) {
+        this.warna = warna;
+    }
+    
+    abstract float getLuas();
+    
+}
